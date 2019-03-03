8581c947913b05ef7066f58dddfccaa52822021d
mark <782355995@qq.com>
2019-03-03 09:13:04 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <!-- 调用Element UI组件 -->
+    <div class="title">登录</div>
+    <div class="main">
+      <el-row>
+        <span>账号</span><el-input placeholder="请输入账号" v-model="username" clearable></el-input>
+        <span>密码</span><el-input type="password" v-model="password" clearable></el-input>
+      </el-row>
+     </div>
+    <div class="buttons">
+      <el-row>           
+        <el-button type="primary" class="submit" @click="handleSunbmit">提交</el-button>
+        <el-button class="clearable" @click="handleClear">重置</el-button>
+      </el-row> 
+    </div>
+    
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  // mounted(){
+  //   //请求数据
+  //   this.$axios.get('https://api.github.com/users').then((res)=>{
+  //     console.log(res.data)
+  //   })
+  // }
+   data() {
+      return {   
+           username: '',
+           password: ''
+      }
+      
+    },
+    methods:{
+      handleSunbmit:function(){
+        // console.log(this.username)
+        var username=this.username 
+        var password=this.password
+        alert('账户：'+username+'密码：'+password)
+      },
+      handleClear:function(){
+        this.username=''
+        this.password=''
+      }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app{
+  width: 500px;
+  margin: 200px auto
+}
+.title{
+  padding: 10px 0;
+ color:#409eff;
+ font-size: 16px;
+ text-align: center;
+}
+.main el-row{
+display: flex;
+width: 100%;
+}
+
+.main span{
+  display: flex;
+ width: 20%;
+}
+.main el-input{
+  display: flex;
+   width: 80%;
+}
+.buttons{
+ padding-left: 200px;
+ padding-top: 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+// 1.引入element-ui
+import ElementUI from 'element-ui';
+// 2.引入element-ui样式
+import 'element-ui/lib/theme-chalk/index.css';
 
+//引入axios
+import axios from 'axios';
+// 3.全局注册组件
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false
+    //吧axios绑定到vue的原型链
+Vue.prototype.$axios = axios;
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
